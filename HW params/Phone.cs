07758de4daa00ed4595228286b24d03b62f01ffe
orianM <86421120+orianM@users.noreply.github.com>
2021-07-30 15:42:02 +0300

    Add files via upload

---
+++ HW params/Phone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_params
+{
+    class Phone
+    {
+        public float screenSize;
+        public string model;
+        public int battery;
+        public string cameraQuality;
+
+        public Phone(float screenSize = 16.9F, string model = "NotNokia", int battery = 50, string cameraQuality = "720p")
+        {
+            this.screenSize = screenSize;
+            this.model = model;
+            this.battery = battery;
+            this.cameraQuality = cameraQuality;
+        }
+    }
+}
---
+++ HW params/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region question 1
+
+            Console.WriteLine(SumOfParams(5, 3, 7, 8));
+
+            #endregion
+
+            #region question 2
+
+            Console.WriteLine($"\n{CheckABC(7)}");
+            Console.WriteLine($"{CheckABC(7, 5)}");
+            Console.WriteLine($"{CheckABC(7, 5, 3)}");
+
+            #endregion
+
+            #region question 3
+
+            Console.WriteLine($"\n{CheckABC(a: 1, c: 10)}");
+
+            #endregion
+
+            #region question 4
+
+            Console.WriteLine($"{MultiplyNumBy3(5)}");
+
+            #endregion
+
+            #region question 5
+
+            Phone myPhone1 = new Phone(battery: 70);
+            Phone myPhone2 = new Phone(model: "WowPhone");
+            Phone myPhone3 = new Phone(screenSize: 14.2F);
+            Phone myPhone4 = new Phone(cameraQuality: "480p");
+
+            #endregion
+        }
+
+        public static int SumOfParams(params int[] myNumbers)
+        {
+            int sum = 0;
+            int howManyNum = myNumbers.Length;
+            for (int i = 0; i < myNumbers.Length; i++)
+            {
+                switch (Array.IndexOf(myNumbers, myNumbers[i]))
+                {
+                    case 0:
+                        sum += myNumbers[i];
+                        break;
+                    case 1:
+                        sum += myNumbers[i] * myNumbers[i];
+                        break;
+                    case 2:
+                        sum += myNumbers[i] * myNumbers[i] * myNumbers[i];
+                        break;
+                    case 3:
+                        sum += myNumbers[i] * myNumbers[i] * myNumbers[i] * myNumbers[i];
+                        break;
+                }
+            }
+            return sum;
+        }
+
+        public static int CheckABC(int a, int b = 1, int c = -1)
+        {
+            return a * b * c;
+        }
+
+        public static int MultiplyNumBy3(in int userNum)
+        {
+            return userNum * 3;
+        }
+    }
+}
